00e1f57c4c2a17dcd9f24003f0c2a8dfba35e4aa
walter <walter.sutter@meticulous.ch>
2026-06-04 17:35:49 +0200

    NO-ISSUE Additional DLQs

--- src/main/java/ch/studior2/buildingpermitmonitor/persistence/BuildingPermitPersistenceApplication.java
+++ src/main/java/ch/studior2/buildingpermitmonitor/persistence/BuildingPermitPersistenceApplication.java
@@ -1,8 +1,11 @@
 package ch.studior2.buildingpermitmonitor.persistence;
 
+import ch.studior2.buildingpermitmonitor.contracts.config.KafkaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(KafkaConfig.class)
 @SpringBootApplication
 public class BuildingPermitPersistenceApplication {
 
